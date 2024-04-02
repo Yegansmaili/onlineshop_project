a9b89c5ae0986bf6a04e0c0556717d9ebcffd3ca
Yegansmaili <yegansmaili@gmail.com>
2024-04-02 22:06:56 +0330

    creat accounts app

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    #local apps
+    'accounts',
 ]
 
 ROOT_URLCONF = 'config.urls'
